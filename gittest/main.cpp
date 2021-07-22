e95c29c537ead91da05af6ebfc260ae9f594ad33
OlgaPolevik <Olga.Polevic@gmail.com>
2021-07-22 23:12:34 +0300

    Add all my files

---
+++ gittest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by Ольга Полевик on 22.07.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
